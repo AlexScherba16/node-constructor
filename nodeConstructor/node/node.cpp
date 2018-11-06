4965be66e333adfcf1663a7fd8bc298b02ba15cf
Alex Scherba <alexscherba16@gmail.com>
2018-11-06 09:41:26 +0300

    Refactor Node base class
    update implementation mouse events

--- nodeConstructor/node/node.cpp
+++ nodeConstructor/node/node.cpp
@@ -1,6 +1,74 @@
 #include "node.h"
 
-//Node::Node(NodeModel *model)//QObject *parent) : QObject(parent)
-//{
+Node* Node::isInsideBounding(const QList<QGraphicsItem *> itemsList){
+    if(itemsList.isEmpty())
+        return nullptr;
 
-//}
+    auto begin = itemsList.constBegin();
+    auto end = itemsList.constEnd();
+
+    while(begin != end){
+        if(((Node*)*begin)->nodeType() == BOUNDING_NODE)
+            return ((Node*)*begin);
+        ++begin;
+    }
+    return nullptr;
+}
+
+void Node::mousePressEvent(QGraphicsSceneMouseEvent *event){
+    update();
+    QGraphicsItemGroup::mousePressEvent(event);
+}
+
+void Node::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+    if(state().isResizing()){
+        prepareGeometryChange();
+        auto oldSize = QSize(geometry().width(), geometry().height());
+        auto diff = event->pos() - event->lastPos();
+        oldSize += QSize(diff.x(), diff.y());
+
+        geometry().setWidth(oldSize.width());
+        geometry().setHeight(oldSize.height());
+
+        recalculatePrimitivesSize();
+        geometry().recalculateSize();
+
+        update();
+        event->accept();
+    }
+    else{
+        QPointF diff = event->scenePos() - event->lastScenePos();
+        moveBy(diff.x(), diff.y());
+//        QGraphicsItemGroup::mouseMoveEvent(event);
+    }
+    QRectF r = scene()->sceneRect();
+    r = r.united(mapToScene(boundingRect()).boundingRect());
+    scene()->setSceneRect(r);
+//    QGraphicsItemGroup::mouseMoveEvent(event);
+}
+
+void Node::hoverMoveEvent(QGraphicsSceneHoverEvent *event){
+    bool resizeResult = false;
+    if (geometry().resizeMarkersContainsMouse(event->pos()) && isSelected()){
+        resizeResult = true;
+        setCursor(QCursor(Qt::SizeFDiagCursor));
+    }
+    else
+        setCursor(QCursor());
+
+    state().setResizingState(resizeResult);
+    QGraphicsItemGroup::hoverMoveEvent(event);
+}
+
+void Node::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event){
+    invokeNodeDelegate();
+    QGraphicsItemGroup::mouseDoubleClickEvent(event);
+}
+
+
+
+void Node::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
+    Q_UNUSED(widget);
+    NodePainter::paintNode(painter, option, *this);
+}
--- nodeConstructor/node/node.h
+++ nodeConstructor/node/node.h
@@ -1,12 +1,81 @@
-#ifndef NODE_H
-#define NODE_H
+#pragma once
+
+/*
+ *
+{
+  "FlowViewStyle": {
+    "BackgroundColor": [53, 53, 53],
+    "FineGridColor": [60, 60, 60],
+    "CoarseGridColor": [25, 25, 25]
+  },
+  "NodeStyle": {
+    "NormalBoundaryColor": [255, 255, 255],
+    "SelectedBoundaryColor": [255, 165, 0],
+    "GradientColor0": "gray",
+    "GradientColor1": [80, 80, 80],
+    "GradientColor2": [64, 64, 64],
+    "GradientColor3": [58, 58, 58],
+    "ShadowColor": [20, 20, 20],
+    "FontColor" : "white",
+    "FontColorFaded" : "gray",
+    "ConnectionPointColor": [169, 169, 169],
+    "FilledConnectionPointColor": "cyan",
+    "ErrorColor": "red",
+    "WarningColor": [128, 128, 0],
+
+    "PenWidth": 1.0,
+    "HoveredPenWidth": 1.5,
+
+    "ConnectionPointDiameter": 8.0,
+
+    "Opacity": 0.8
+  },
+  "ConnectionStyle": {
+    "ConstructionColor": "gray",
+    "NormalColor": "darkcyan",
+    "SelectedColor": [100, 100, 100],
+    "SelectedHaloColor": "orange",
+    "HoveredColor": "lightcyan",
+
+    "LineWidth": 3.0,
+    "ConstructionLineWidth": 2.0,
+    "PointDiameter": 10.0,
+
+    "UseDataDefinedColors": false
+  }
+}
+ *
+ *
+ */
+
+
+
+
+
+
+
+
+
 
 //#include <QObject>
 #include <QGraphicsItemGroup>
+#include <QGraphicsSceneMouseEvent>
+#include <QStyleOptionGraphicsItem>
+#include <QGraphicsScene>
 
 #include "state/state.h"
 #include "geometry/geometry.h"
 #include <QDebug>
+#include <QPen>
+#include "stdutil.h"
+
+#include "nodePainter/nodepainter.h"
+
+typedef enum{
+    DEFAULT_NODE = 0,
+    BOUNDING_NODE,
+}TNodeType;
+
 
 class NodeModel;
 class Node : public QGraphicsItemGroup
@@ -14,18 +83,19 @@ class Node : public QGraphicsItemGroup
 //    Q_OBJECT
 public:
     Node() = delete;
+//    Node(std::unique_ptr<NodeModel> && dataModel);
     Node(NodeModel* model) : QGraphicsItemGroup(),
+                            _nodeModel(nullptr),
                             _nodeGeometry(Geometry()),
                             _nodeState(State())
 
     {
         Q_UNUSED(model);
-        qDebug() << __PRETTY_FUNCTION__;
-
     }
     virtual ~Node() {}
 
-    virtual void generateGui()  = 0;
+    virtual void generateGui()          = 0;
+    virtual void invokeNodeDelegate()   = 0;
     virtual void updateNodeUi() {}
 
     State& state() {return _nodeState;}
@@ -34,13 +104,32 @@ public:
     Geometry& geometry() {return _nodeGeometry;}
     const Geometry& geometry() const {return _nodeGeometry;}
 
+    QRectF boundingRect() const {return geometry().nodeBoundingRect();}
+    virtual TNodeType nodeType() {return DEFAULT_NODE;}
+
+//    bool contains(const QPointF &point) {} const override;
+
 protected:
+
+    Node *isInsideBounding(const QList<QGraphicsItem *> itemsList);
+
+    void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
+    void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override;
+    void hoverMoveEvent(QGraphicsSceneHoverEvent* event) override;
+    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) override;
+
+    void paint(QPainter*                       painter,
+          QStyleOptionGraphicsItem const* option,
+          QWidget*                        widget = 0) override;
+
+
 
     virtual void recalculatePrimitivesSize() = 0;
 
-    Geometry            _nodeGeometry;
-    State               _nodeState;
+//    std::unique_ptr<NodeModel>  _nodeModel;
+    NodeModel*                  _nodeModel;
+    Geometry                    _nodeGeometry;
+    State                       _nodeState;
 
 };
 
-#endif // NODE_H
